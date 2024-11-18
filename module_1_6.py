6b1cf53163c306dafff693da40c4a46c374f12e4
genn125 <genn125@gmail.com>
2024-11-18 22:21:09 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+
+# Практика: "Переменные"
+
+number_of_completed_DZ = 12.0
+
+hours_spent = 1.5
+
+course_name = 'Python'
+
+time_one_task = hours_spent / number_of_completed_DZ
+
+print ('Курс:' , course_name+',' , 'всего задач:' , number_of_completed_DZ , ', затрачено часов:' , hours_spent , ', среднее время выполнения:' , time_one_task , 'часов.')
+
+
+# Как поставить запятую БЕЗ пробела после 12.0 и 1.5???
