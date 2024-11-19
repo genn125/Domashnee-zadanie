b690f0bfc7024c7d0f586bf95ef825c53054ff56
genn125 <genn125@gmail.com>
2024-11-19 14:00:19 +0300

    Add files via upload

---
+++ module_1_3_1.py
@@ -0,0 +1,14 @@
+
+# module_1_3_1
+
+# Практика: "Переменные"
+
+number_of_completed_DZ = 12.0
+
+hours_spent = 1.5
+
+course_name = 'Python'
+
+time_one_task = hours_spent / number_of_completed_DZ
+
+print ('Курс:' , course_name+',' , 'всего задач:' , number_of_completed_DZ , ', затрачено часов:' , hours_spent , ', среднее время выполнения:' , time_one_task , 'часов.')
