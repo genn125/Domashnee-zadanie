6212a33c97ab21d514153183a63621e31a0253de
genn125 <genn125@gmail.com>
2024-11-22 09:45:56 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,15 +1,16 @@
 
-# Практика: "Переменные"
+#module_1_6.py
 
-number_of_completed_DZ = 12.0
+#1_6 "Словари и множества"
 
-hours_spent = 1.5
+my_dict = {ВРУ 10 : 1-3-8 ,
+ЦХМ СТ : 1-3-9,
+ВРУ 1 : 1-3-10,
+ВРУ 2 : 1-1-1,
+ЦХМ НТ : 1-1-2,
+ВРУ 11 : 1-1-3,
 
-course_name = 'Python'
-
-time_one_task = hours_spent / number_of_completed_DZ
-
-print ('Курс:' , course_name+',' , 'всего задач:' , number_of_completed_DZ , ', затрачено часов:' , hours_spent , ', среднее время выполнения:' , time_one_task , 'часов.')
+}
 
 
-# Как поставить запятую БЕЗ пробела после 12.0 и 1.5???
+
